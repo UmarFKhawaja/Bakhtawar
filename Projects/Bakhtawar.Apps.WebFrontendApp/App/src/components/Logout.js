6ffbd2a8bc6762355a205e4492f8e0a0b2c98004
Umar Farooq Khawaja <khawaja.umar.farooq@gmail.com>
2021-02-01 02:12:00 +0000

    Clean up React code.

--- Projects/Bakhtawar.Apps.WebFrontendApp/App/src/components/Logout.js
+++ Projects/Bakhtawar.Apps.WebFrontendApp/App/src/components/Logout.js
@@ -1,127 +1,143 @@
-import React from 'react'
-import { Component } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { QueryParameterNames, LogoutActions, ApplicationPaths } from 'constants/Authorization';
 import { AuthenticationResultStatus, authorizationManager } from 'services/authorization-manager';
 
 // The main responsibility of this component is to handle the user's logout process.
 // This is the starting point for the logout process, which is usually initiated when a
 // user clicks on the logout button on the LoginMenu component.
-export class Logout extends Component {
-    constructor(props) {
-        super(props);
+export const Logout = ({ action }) => {
+  const [isReady, setIsReady] = useState(false);
+  const [message, setMessage] = useState();
 
-        this.state = {
-            message: undefined,
-            isReady: false,
-            authenticated: false
-        };
+  const getReturnUrl = useCallback((state) => {
+    const params = new URLSearchParams(window.location.search);
+    const fromQuery = params.get(QueryParameterNames.ReturnUrl);
+
+    if (fromQuery && !fromQuery.startsWith(`${window.location.origin}/`)) {
+      // This is an extra check to prevent open redirects.
+      throw new Error('Invalid return url. The return url needs to have the same origin as the current page.');
     }
 
-    componentDidMount() {
-        const action = this.props.action;
-        switch (action) {
-            case LogoutActions.Logout:
-                if (!!window.history.state.state.local) {
-                    this.logout(this.getReturnUrl());
-                } else {
-                    // This prevents regular links to <app>/authentication/logout from triggering a logout
-                    this.setState({ isReady: true, message: "The logout was not initiated from within the page." });
-                }
-                break;
-            case LogoutActions.LogoutCallback:
-                this.processLogoutCallback();
-                break;
-            case LogoutActions.LoggedOut:
-                this.setState({ isReady: true, message: "You successfully logged out!" });
-                break;
-            default:
-                throw new Error(`Invalid action '${action}'`);
-        }
+    return (state && state.returnUrl) || fromQuery || `${window.location.origin}${ApplicationPaths.LoggedOut}`;
+  }, []);
 
-        this.populateAuthenticationState();
+  const navigateToReturnUrl = useCallback((returnUrl) => window.location.replace(returnUrl), []);
+
+  const logout = useCallback(async (returnUrl) => {
+    const isAuthenticated = await authorizationManager.isAuthenticated();
+
+    if (isAuthenticated) {
+      const result = await authorizationManager.signOut({ returnUrl });
+
+      switch (result.status) {
+        case AuthenticationResultStatus.Redirect:
+          break;
+
+        case AuthenticationResultStatus.Success:
+          await navigateToReturnUrl(returnUrl);
+          break;
+
+        case AuthenticationResultStatus.Fail:
+          setMessage(result.message);
+          break;
+
+        default:
+          throw new Error('Invalid authentication result status.');
+      }
+    } else {
+      setMessage('You successfully logged out!');
     }
+  }, [navigateToReturnUrl]);
 
-    render() {
-        const { isReady, message } = this.state;
-        if (!isReady) {
-            return <div></div>
-        }
-        if (!!message) {
-            return (<div>{message}</div>);
+  const processLogoutCallback = useCallback(async () => {
+    const url = window.location.href;
+    const result = await authorizationManager.completeSignOut(url);
+
+    switch (result.status) {
+      case AuthenticationResultStatus.Redirect:
+        // There should not be any redirects as the only time completeAuthentication finishes
+        // is when we are doing a redirect sign in flow.
+        throw new Error('Should not redirect.');
+
+      case AuthenticationResultStatus.Success:
+        await navigateToReturnUrl(getReturnUrl(result.state));
+        break;
+
+      case AuthenticationResultStatus.Fail:
+        setMessage(result.message);
+        break;
+
+      default:
+        throw new Error(`Invalid authentication result status '${result.status}'.`);
+    }
+  }, [getReturnUrl, navigateToReturnUrl]);
+
+  const populateAuthenticationState = useCallback(async () => {
+    await authorizationManager.isAuthenticated();
+
+    setIsReady(true);
+  }, []);
+
+  useEffect(() => {
+    switch (action) {
+      case LogoutActions.Logout:
+        if (!!window.history.state.state.local) {
+          logout(getReturnUrl()).then();
         } else {
-            const action = this.props.action;
-            switch (action) {
-                case LogoutActions.Logout:
-                    return (<div>Processing logout</div>);
-                case LogoutActions.LogoutCallback:
-                    return (<div>Processing logout callback</div>);
-                case LogoutActions.LoggedOut:
-                    return (<div>{message}</div>);
-                default:
-                    throw new Error(`Invalid action '${action}'`);
-            }
+          // This prevents regular links to <app>/authentication/logout from triggering a logout
+          setIsReady(true);
+          setMessage('The logout was not initiated from within the page.');
         }
+        break;
+
+      case LogoutActions.LogoutCallback:
+        processLogoutCallback().then();
+        break;
+
+      case LogoutActions.LoggedOut:
+        setIsReady(true);
+        setMessage('You successfully logged out!');
+        break;
+
+      default:
+        throw new Error(`Invalid action '${action}'`);
     }
 
-    async logout(returnUrl) {
-        const state = { returnUrl };
-        const isauthenticated = await authorizationManager.isAuthenticated();
-        if (isauthenticated) {
-            const result = await authorizationManager.signOut(state);
-            switch (result.status) {
-                case AuthenticationResultStatus.Redirect:
-                    break;
-                case AuthenticationResultStatus.Success:
-                    await this.navigateToReturnUrl(returnUrl);
-                    break;
-                case AuthenticationResultStatus.Fail:
-                    this.setState({ message: result.message });
-                    break;
-                default:
-                    throw new Error("Invalid authentication result status.");
-            }
-        } else {
-            this.setState({ message: "You successfully logged out!" });
-        }
-    }
+    populateAuthenticationState().then();
 
-    async processLogoutCallback() {
-        const url = window.location.href;
-        const result = await authorizationManager.completeSignOut(url);
-        switch (result.status) {
-            case AuthenticationResultStatus.Redirect:
-                // There should not be any redirects as the only time completeAuthentication finishes
-                // is when we are doing a redirect sign in flow.
-                throw new Error('Should not redirect.');
-            case AuthenticationResultStatus.Success:
-                await this.navigateToReturnUrl(this.getReturnUrl(result.state));
-                break;
-            case AuthenticationResultStatus.Fail:
-                this.setState({ message: result.message });
-                break;
-            default:
-                throw new Error("Invalid authentication result status.");
-        }
-    }
+    return () => {
+    };
+  }, [action, getReturnUrl, logout, populateAuthenticationState, processLogoutCallback]);
 
-    async populateAuthenticationState() {
-        const authenticated = await authorizationManager.isAuthenticated();
-        this.setState({ isReady: true, authenticated });
-    }
+  if (!isReady) {
+    return (
+      <div/>
+    );
+  }
 
-    getReturnUrl(state) {
-        const params = new URLSearchParams(window.location.search);
-        const fromQuery = params.get(QueryParameterNames.ReturnUrl);
-        if (fromQuery && !fromQuery.startsWith(`${window.location.origin}/`)) {
-            // This is an extra check to prevent open redirects.
-            throw new Error("Invalid return url. The return url needs to have the same origin as the current page.")
-        }
-        return (state && state.returnUrl) ||
-            fromQuery ||
-            `${window.location.origin}${ApplicationPaths.LoggedOut}`;
-    }
+  if (!!message) {
+    return (
+      <div>{message}</div>
+    );
+  } else {
+    switch (action) {
+      case LogoutActions.Logout:
+        return (
+          <div>Processing logout</div>
+        );
 
-    navigateToReturnUrl(returnUrl) {
-        return window.location.replace(returnUrl);
+      case LogoutActions.LogoutCallback:
+        return (
+          <div>Processing logout callback</div>
+        );
+
+      case LogoutActions.LoggedOut:
+        return (
+          <div>{message}</div>
+        );
+
+      default:
+        throw new Error(`Invalid action '${action}'`);
     }
-}
+  }
+};
